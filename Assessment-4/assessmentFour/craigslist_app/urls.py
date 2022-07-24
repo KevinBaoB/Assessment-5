d5bd8cba05a1de443f1c2db933ac20767911c91a
KevinBaoB <kevinbaobui@gmail.com>
2022-07-24 16:07:10 -0500

    Needs touch-ups

--- Assessment-4/assessmentFour/craigslist_app/urls.py
+++ Assessment-4/assessmentFour/craigslist_app/urls.py
@@ -3,8 +3,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, ),
+    path('', views.home, name='home'),
+
     path('<int:category_id>/', views.list_posts, name = "list_posts"),
+
+    path('add_category/', views.add_category, name='add_category'),
+
+    path('<int:category_id>/edit_category/', views.edit_category, name='edit_category'),
+
+    path('<int:category_id>/delete_category', views.delete_category, name='delete_category'),
+
     path('<int:category_id>/posts/<int:post_id>/', views.post_info, name='post_info'),
+
     path('<int:category_id>/posts/new',views.add_post, name='add_post'),
+
+    path('<int:category_id>/posts/<int:post_id>/edit', views.edit_post, name = 'edit_post'),
+
+    path('<int:category_id>/posts/<int:post_id>/delete', views.delete_post, name='delete_post')
 ]
--- Assessment-4/assessmentFour/craigslist_app/views.py
+++ Assessment-4/assessmentFour/craigslist_app/views.py
@@ -1,8 +1,11 @@
-from django.http import JsonResponse
-from django.shortcuts import render
+from cmd import PROMPT
+from unicodedata import category
+from django.http import HttpResponseRedirect, JsonResponse
+from django.shortcuts import get_object_or_404, render
 from .models import Category, Post
 from django.views.decorators.csrf import csrf_exempt
 import json
+from django.urls import reverse
 
 # Create your views here.
 def home(request) :
@@ -10,6 +13,37 @@ def home(request) :
     data = {"categories": categories}
 
     return render(request, 'craigslist_app/home.html', data)
+
+@csrf_exempt
+def add_category(request):
+    if request.method == "POST":
+        body = json.loads(request.body)
+        newCategory = Category(name = body['name'])
+        newCategory.save()
+        return JsonResponse({})
+    return render(request, "craigslist_app/add_category.html")
+
+@csrf_exempt
+def edit_category(request, category_id):
+    if request.method == "POST":
+        body = json.loads(request.body)
+        category = Category.objects.all().get(id = category_id)
+
+        category.name = body["name"]
+
+        category.save()
+        return JsonResponse({})
+    data = {'category': Category.objects.all().get(id = category_id)}
+    return render(request, "craigslist_app/edit_category.html", data)
+
+@csrf_exempt
+def delete_category(request, category_id):
+    category = get_object_or_404(Category, id = category_id)
+    if request.method == "POST":
+        category.delete()
+        return HttpResponseRedirect(reverse("home"))
+    
+    return render(request, 'craigslist_app/delete_category.html')
 
 def list_posts(request, category_id):
     category_posts = Post.objects.all().filter(category_id = category_id)
@@ -21,6 +55,16 @@ def post_info(request, category_id, post_id):
     post = Post.objects.all().get(id = post_id)
     data = {"post": post, 'category_id':category_id}
     return render(request, 'craigslist_app/post_info.html', data)
+
+@csrf_exempt
+def delete_post(request, category_id, post_id):
+    post = get_object_or_404(Post, id = post_id)
+    if request.method == "POST":
+        post.delete()
+        PROMPT('Delete Successful')
+        return HttpResponseRedirect(reverse("home"))
+    
+    return render(request, 'craigslist_app/delete_post.html')
     
 @csrf_exempt
 def add_post(request, category_id):
@@ -30,3 +74,18 @@ def add_post(request, category_id):
         newPost.save()
         return JsonResponse({})
     return render(request, "craigslist_app/add_post.html")
+
+@csrf_exempt
+def edit_post(request, category_id, post_id):
+    if request.method == "POST":
+        body = json.loads(request.body)
+        post = Post.objects.all().get(id = post_id)
+
+        post.title = body['title']
+        post.user = body['user']
+        post.description = body['description']
+
+        post.save()
+        return JsonResponse({})
+    data = {'post': Post.objects.all().get(id = post_id)}
+    return render(request, "craigslist_app/edit_post.html", data)
